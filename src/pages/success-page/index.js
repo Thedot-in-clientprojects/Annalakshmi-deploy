e7241d362d6ddff7f91c82c1e70a841c2f035d4d
Varun <nurav2037@gmial.com>
2022-12-11 16:03:30 +0530

    working on: success page

--- src/pages/success-page/index.js
+++ src/pages/success-page/index.js
@@ -1,54 +1,75 @@
+/* eslint-disable @next/next/no-css-tags */
+import { useEffect } from 'react';
 import Head from 'next/head';
-import React from 'react'
-import Navbar from '../../components/Admin/Shared/Navbar'
-import Header from '../../components/Resturant/Header';
+//= Layout
 import AppLayout from '../../layouts/app-layout';
+//= Components
+import Header from '../../components/Resturant/Header';
+import Features from '../../components/Resturant/Features';
+import Hero from '../../components/Resturant/Hero';
+import Menu from '../../components/Resturant/Menu';
+import Services from '../../components/Resturant/Services';
+import Gallery from '../../components/Resturant/Gallery';
+import Testimonials from '../../components/Resturant/Testimonials';
+import CallAction from '../../components/Resturant/CallAction';
+import Blog from '../../components/Resturant/Blog';
 import Footer from '../../components/Footers/RestaurantFooter';
+import { Container } from '@mui/material';
+import Link from 'next/link';
 
-
-function SuccessPage() {
-
-
-    const nav_links = [
-        {
-          name: "About",
-          href: "/about-business"
-        },
-        {
-          name: "Contact Us",
-          href: "#0"
-        },
-        {
-          name: "Menu",
-          href: "#0"
-        },
-        {
-          name: "Gallery",
-          href: "#0"
-        },
-        
-      ]
+const SuccessPage = () => {
+  useEffect(() => {
+    document.body.classList.add('index-restaurant');
     
+    const removeClasses = [
+      'index-bus1', 'index-corporate',
+      'index-freelancer', 'nft-market',
+      'cr-agency', 'mobile-app', 'gr-orange-bg',
+      'land-demo2', 'index-main', 'index-arch'
+    ];
 
+    document.body.classList.remove(...removeClasses);
+  }, []);
+
+  const nav_links = [
+    {
+      name: "About",
+      href: "/about-business"
+    },
+    {
+      name: "Contact Us",
+      href: "#0"
+    },
+    {
+      name: "Menu",
+      href: "#0"
+    },
+    {
+      name: "Gallery",
+      href: "#0"
+    },
+    
+  ]
 
   return (
-    <div>
-    <Head>
+    <>
+      <Head>
         <title>Annalakshmi</title>
         <link rel="icon" href="/favicon.ico" />
-        <title>Annalakshmi Restaurant - Booking Success</title>
+        <title>Annalakshmi Restaurant</title>
       </Head>
       <AppLayout type="restaurant" links={nav_links} navTheme="dark">
-        <main className="position-re">
-            <h1>
-                Thank You, We will reach out to you soon :-)
-            </h1>
-          <Footer />
-        </main>
+      <header style={{paddingTop: 80}}   data-scroll-index="0">
+          <div className='header-main'>
+            <Container style={{color:"#fff"}}>
+              <h1>Your Reservation is Done!</h1><br/><h2>We will get back to you soon</h2>
+              <Link href="/landing-preview"><button className="header-main-btn" style={{marginLeft: "-1px"}}>Back to Home  </button></Link>
+            </Container>
+          </div> 
+          </header>
       </AppLayout>
-    
-    </div>
+    </>
   )
 }
 
-export default SuccessPage
+export default SuccessPage;
